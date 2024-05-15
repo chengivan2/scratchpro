c006bf50ef9988fca760ccdde2b924f08e812dbb
chengivan <chengoleivan@gmail.com>
2024-05-16 02:57:16 +0600

    Fix a million and one

--- src/app/dashboard/IncompleteTodos.tsx
+++ src/app/dashboard/IncompleteTodos.tsx
@@ -1,41 +1,52 @@
-"use server";
+"use client";
 
+import React, { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-export default async function IncompleteTodos() {
+interface Todo {
+  todoid: number;
+  todoname: string;
+  tododescription: string;
+  completed: boolean;
+  timeadded: Date;
+  todouserid: number;
+}
+
+export default function IncompleteTodos() {
+  const [todos, setTodos] = useState<Todo[]>([]);
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 
-  // Select the user's todos
-  async function fetchTodos() {
-    // // Get the current user
+  useEffect(() => {
+    fetchTodos();
+  }, []);
 
+  async function fetchTodos() {
     // const {
     //   data: { user },
     // } = await supabase.auth.getUser();
-
-    // // Get the user's ID
     // const userid = user?.id;
-    const { data: incompleteTodosData, error } = await supabase
+    const { data: todos, error } = await supabase
       .from("current_todos")
       .select("*")
       .eq("completed", false);
-    //   .eq("todouserid", userid)
+    //   .eq("todouserid", user?.id)
 
-    if (error) {
-      console.log("Error: ", error);
-    } else {
-      console.log("Incomplete Todos: ", incompleteTodosData);
-    }
+    if (error) console.log("Error: ", error);
+    else setTodos(todos || []);
   }
-
-  fetchTodos();
 
   return (
     <div>
-      <h2>IncompleteTodos</h2>
+      <h2>Incomplete Todos</h2>
+      {todos.map((todo) => (
+        <div key={todo.todoid}>
+          <h2>{todo.todoname}</h2>
+          <p>{todo.tododescription}</p>
+        </div>
+      ))}
     </div>
   );
 }
